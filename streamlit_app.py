a730cb5b9a5581a3a83aad292ee9959376bdb32c
earndigital250 <159711441+earndigital250@users.noreply.github.com>
2024-02-18 20:13:40 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.llms import OpenAI
-st.set_page_config(page_title="🦜🔗 Quickstart App")
-st.title('🦜🔗 Quickstart App')
+st.set_page_config(page_title="Saylani LLM Assistant🤖")
+st.title('Saylani LLM Assistant🤖')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
